55b939c133830e94def1dbfab21ddafad137cbb9
Alex Kalaverin <alex@kalaver.in>
2026-04-02 17:14:04 +0300

    annotations: some annotations

--- src/kain/classes.py
+++ src/kain/classes.py
@@ -1,32 +1,45 @@
+from typing import Any, override
+
 from kain.internals import Who
 
 __all__ = "Missing", "Nothing", "Singleton"
 
 
-class Singleton(type):
-
-    def __init__(cls, name, parents, attrbutes):
-        super().__init__(name, parents, attrbutes)
-
-    def __call__(cls, *args, **kw):
-        if not hasattr(cls, "instance"):
-            cls.instance = super().__call__(*args, **kw)
-        return cls.instance
-
-
 class Missing:
 
-    def __hash__(self):
+    @override
+    def __hash__(self) -> int:
         return id(self)
 
-    def __bool__(self):
+    @override
+    def __bool__(self) -> bool:
         return False
 
-    def __eq__(self, _):
+    @override
+    def __eq__(self, _) -> bool:
         return False
 
-    def __repr__(self):
+    @override
+    def __repr__(self) -> str:
         return f"<{Who.Name(self, addr=True)}>"
 
 
 Nothing = Missing()
+
+
+class Singleton(type):
+
+    def __init__(
+        cls,
+        name: str,
+        parents: tuple[()] | tuple[type] | tuple[type, ...],
+        attributes: dict[str, Any],
+    ) -> None:
+        cls.instance: Any | Missing = Nothing
+        super().__init__(name, parents, attributes)
+
+    @override
+    def __call__(cls, *args: Any, **kw: dict[str, Any]) -> Any:
+        if cls.instance is not Missing:
+            cls.instance = super().__call__(*args, **kw)
+        return cls.instance
